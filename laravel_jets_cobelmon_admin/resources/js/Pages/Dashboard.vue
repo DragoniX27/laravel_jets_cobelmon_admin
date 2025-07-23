762fdf4af963b46853a16cb316d994a8bf5e9c6c
Santiago Giraldo Pulgarin <santiago.giraldo@linktic.com>
2025-07-23 15:41:26 -0500

    Agregndo Modificaciones Para conectar con MongoDB

--- laravel_jets_cobelmon_admin/resources/js/Pages/Dashboard.vue
+++ laravel_jets_cobelmon_admin/resources/js/Pages/Dashboard.vue
@@ -9,16 +9,82 @@
         <div class="py-10">
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-                    <template v-if="$page.props.auth">
+                    <template v-if="$page.props.is_admin">
                         <div
                             class="bg-gradient-to-br from-gray-800 to-gray-900 p-6 rounded-xl shadow-lg hover:scale-105 transition transform cursor-pointer border border-gray-700 hover:border-indigo-500 flex items-center justify-between gap-6"
                         >
                             <div class="flex items-center gap-4 flex-shrink-0">
                                 <img :src="$page.props.globalUrl + '/imgs/libreria.webp'" alt="Guías" class="w-20 h-20 object-contain" />
-                                <h3 class="text-xl font-semibold text-white">Guías</h3>
+                                <h3 class="text-xl font-semibold text-white">Gestion de Jugadores</h3>
                             </div>
                             <p class="text-sm text-gray-300 text-left max-w-md">
-                                {{ $page.props.is_admin }}
+                                Gestiona los Jugadores
+                            </p>
+                        </div>
+                        <div
+                            class="bg-gradient-to-br from-gray-800 to-gray-900 p-6 rounded-xl shadow-lg hover:scale-105 transition transform cursor-pointer border border-gray-700 hover:border-indigo-500 flex items-center justify-between gap-6"
+                        >
+                            <div class="flex items-center gap-4 flex-shrink-0">
+                                <img :src="$page.props.globalUrl + '/imgs/libreria.webp'" alt="Guías" class="w-20 h-20 object-contain" />
+                                <h3 class="text-xl font-semibold text-white">Auras</h3>
+                            </div>
+                            <p class="text-sm text-gray-300 text-left max-w-md">
+                                Gestiona las Auras para los jugadores
+                            </p>
+                        </div>
+                        <div
+                            class="bg-gradient-to-br from-gray-800 to-gray-900 p-6 rounded-xl shadow-lg hover:scale-105 transition transform cursor-pointer border border-gray-700 hover:border-indigo-500 flex items-center justify-between gap-6"
+                        >
+                            <div class="flex items-center gap-4 flex-shrink-0">
+                                <img :src="$page.props.globalUrl + '/imgs/libreria.webp'" alt="Guías" class="w-20 h-20 object-contain" />
+                                <h3 class="text-xl font-semibold text-white">Gestion de Codigos</h3>
+                            </div>
+                            <p class="text-sm text-gray-300 text-left max-w-md">
+                                Gestiona los codigos
+                            </p>
+                        </div>
+                        <div
+                            class="bg-gradient-to-br from-gray-800 to-gray-900 p-6 rounded-xl shadow-lg hover:scale-105 transition transform cursor-pointer border border-gray-700 hover:border-indigo-500 flex items-center justify-between gap-6"
+                        >
+                            <div class="flex items-center gap-4 flex-shrink-0">
+                                <img :src="$page.props.globalUrl + '/imgs/libreria.webp'" alt="Guías" class="w-20 h-20 object-contain" />
+                                <h3 class="text-xl font-semibold text-white">Gestion de Banners</h3>
+                            </div>
+                            <p class="text-sm text-gray-300 text-left max-w-md">
+                                Gestiona los Banners del Gacha
+                            </p>
+                        </div>
+                        <div
+                            class="bg-gradient-to-br from-gray-800 to-gray-900 p-6 rounded-xl shadow-lg hover:scale-105 transition transform cursor-pointer border border-gray-700 hover:border-indigo-500 flex items-center justify-between gap-6"
+                        >
+                            <div class="flex items-center gap-4 flex-shrink-0">
+                                <img :src="$page.props.globalUrl + '/imgs/libreria.webp'" alt="Guías" class="w-20 h-20 object-contain" />
+                                <h3 class="text-xl font-semibold text-white">Gestion de Insignias</h3>
+                            </div>
+                            <p class="text-sm text-gray-300 text-left max-w-md">
+                                Gestiona las Insignias de los jugadores
+                            </p>
+                        </div>
+                        <div
+                            class="bg-gradient-to-br from-gray-800 to-gray-900 p-6 rounded-xl shadow-lg hover:scale-105 transition transform cursor-pointer border border-gray-700 hover:border-indigo-500 flex items-center justify-between gap-6"
+                        >
+                            <div class="flex items-center gap-4 flex-shrink-0">
+                                <img :src="$page.props.globalUrl + '/imgs/libreria.webp'" alt="Guías" class="w-20 h-20 object-contain" />
+                                <h3 class="text-xl font-semibold text-white">Gestion de NPCs</h3>
+                            </div>
+                            <p class="text-sm text-gray-300 text-left max-w-md">
+                                gestiona los NPCs que has creado y sus eventos
+                            </p>
+                        </div>
+                        <div
+                            class="bg-gradient-to-br from-gray-800 to-gray-900 p-6 rounded-xl shadow-lg hover:scale-105 transition transform cursor-pointer border border-gray-700 hover:border-indigo-500 flex items-center justify-between gap-6"
+                        >
+                            <div class="flex items-center gap-4 flex-shrink-0">
+                                <img :src="$page.props.globalUrl + '/imgs/libreria.webp'" alt="Guías" class="w-20 h-20 object-contain" />
+                                <h3 class="text-xl font-semibold text-white">Gestion de Pokemons</h3>
+                            </div>
+                            <p class="text-sm text-gray-300 text-left max-w-md">
+                                Gestiona los pokemons dispon
                             </p>
                         </div>
                     </template>
--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-echo "🚀 Construyendo y levantando los contenedores..."
-docker compose -f stack-compose.yml up --build -d
+echo "🚀 Levantando contenedores necesarios (bd, redis, mongodb, laravel)..."
+docker compose -f stack-compose.yml build --no-cache laravel
+docker compose -f stack-compose.yml up -d redis db mongodb laravel
 
 echo "⏳ Esperando 10 segundos a que la base de datos arranque..."
 sleep 10
@@ -10,15 +11,20 @@ echo "⚙️ Instalando dependencias de Laravel..."
 docker exec -it laravel composer install
 
 echo "⚙️ Instalando dependencias de NPM..."
-docker exec -it laravel npm i
+docker exec -it laravel npm install
 
-echo "⚙️ IBuildeando el fron.."
+echo "⚙️ Construyendo el frontend..."
 docker exec -it laravel npm run build
 
 echo "🔑 Generando clave de aplicación..."
 docker exec -it laravel php artisan key:generate
 
 echo "🧬 Ejecutando migraciones..."
-docker exec -it laravel php artisan migrate
+docker exec -it laravel php artisan migrate:fresh --seed
 
-echo "✅ Listo. Laravel corriendo en http://localhost:8000"
+echo "✅ Laravel corriendo en http://localhost:8000"
+
+echo "🚀 Levantando el servidor de Minecraft..."
+docker compose -f stack-compose.yml up -d minecraft
+
+echo "✅ Minecraft corriendo"
